104a90f8f22182f8bd716e6d18c898a74d3fcb4f
satheesh90 <satheesh.retros@gmail.com>
2016-08-22 13:20:26 +0000

    new comm

--- minis/comm/Health.py
+++ minis/comm/Health.py
@@ -1,66 +1,129 @@
+
 #!/usr/bin/env python
 
-import sh, time
-import socket
+import os, sys
+import time, socket
 
 def Check():
+  name = socket.gethostname()
+  print name
+  i=2
+  while (i<6) :
+        if (i==2):
+                chk = name[i]
+        else:
+                chk = chk + name[i]
+        i=i+1
+  
+  print chk	
+  if chk == "SORT":
+	neigh = open('../config/Neighbours.txt').read().splitlines()
+	inhost = neigh[0]
+        outhost = neigh[1]
+        lefthost = neigh[2]
+       	righthost = neigh[3]
 
-	#name = socket.gethostname()
-	#i=2
-	#while (i<6) :
-        #        if (i==2):
-        #                chk = name[i]
-        #        else:
-        #                chk = chk + name[i]
-        #        i=i+1
-	#s = open('../config/Neighbours.txt','r')
+  	while 1:
+		if (inhost != "None"): 
+                        addrin = inhost + ".local"
+			res1 = os.system("sudo ping "+addrin +" -c 1 -w2 >/dev/null 2>&1")
+			if res1 == 0:
+				print inhost,"is alive"
+				time.sleep(.5)
+			else:
+				print inhost,"is Dead"
+				AlertMail(inhost)
+                                # mongodb update for failure
+				time.sleep(.5)
 
-	#if chk == "SORT":
-	inhost = "ILCURV01"
-	outhost = "ILLONG01"
-	lefthost = "ILLONG01"
-	righthost = "ILDUMM03"
-	x=1
-	while 1:
-	 	if (inhost != "None") and (x==1):
-			addrin = inhost + ".local"
-			try :
-				sh.ping(addrin) # "-c 1", _out="/dev/null")
-				print inhost, "is healthy"
-				x=x+1
-			except sh.ErrorReturnCode_1:
-					print inhost, "is dead"
-					x=x+1
+		if (outhost != "None"): 
+                        addrout = outhost + ".local"
+                        res2 = os.system("sudo ping "+addrout + " -c 1 -w2 >/dev/null 2>&1")
+                        if res2 == 0:
+                                print outhost,"is alive"
+                                time.sleep(.5)
+                        else:
+                                print outhost,"is Dead"
+                                AlertMail(outhost)
+                                # mongodb update for failure
+				time.sleep(.5)
 
-		elif (outhost != "None") and (x==2):
-                                addrout = outhost + ".local"
-                                try :
-                                        sh.ping(addrin, "-c 1", _out="/dev/null")
-                                        print outhost, "is healthy"
-					x=x+1
-				except sh.ErrorReturnCode_1:
-                                        print outhost, "is dead"
-					x=x+1
+                if (lefthost != "None"): 
+                        addrleft = lefthost + ".local"
+                        res3 = os.system("sudo ping "+addrleft + " -c 1 -w2 >/dev/null 2>&1")
+                        if res3 == 0:
+                                print lefthost,"is alive"
+                                time.sleep(.5)
+                        else:
+                                print lefthost,"is Dead"
+                                AlertMail(lefthost)
+                                # mongodb update for failure
+				time.sleep(.5)
 
-		elif (lefthost != "None") and (x==3):
-                                addrleft = lefthost + ".local"
-                                try :
-                                        sh.ping(addrleft, "-c 1", _out="/dev/null")
-                                        print lefthost, "is healthy"
-					x=x+1
-				except sh.ErrorReturnCode_1:
-                                        print lefthost, "is dead"
-					x=x+1
-		elif (righthost != "None") and (x==4):
-                                addrright = righthost + ".local"
-                                try :
-                                        sh.ping(addrright, "-c 1", _out="/dev/null")
-                                        print righthost, "is healthy"
-					x=1
-				except sh.ErrorReturnCode_1:
-                                        print righthost, "is dead"
-					x=1
+                if (righthost != "None"):
+                        addrright = righthost + ".local"
+                        res4 = os.system("sudo ping "+addrright + " -c 1 -w2 >/dev/null 2>&1")
+                        if res4 == 0:
+                                print righthost,"is alive"
+                                time.sleep(.5)
+                        else:
+                                print righthost,"is Dead"
+                                AlertMail(righthost)
+                                # mongodb update for failure
+				time.sleep(.5)
 
-#def AlertEmail()
+
+
+
+  else: 
+        neigh = open('../config/Neighbours.txt').read().splitlines()
+        inhost = neigh[0]
+        outhost = neigh[1]
+        
+        while 1:
+                if (inhost != "None"):
+                        addrin = inhost + ".local"
+                        res1 = os.system("sudo ping "+addrin +" -c 1 -w2 >/dev/null 2>&1")
+                        if res1 == 0:
+                                print inhost,"is alive"
+                                time.sleep(.5)
+                        else:
+                                print inhost,"is Dead"
+  	                        AlertMail(inhost)
+                                # mongodb update for failure
+				time.sleep(.5)
+
+                if (outhost != "None"):
+                        addrout = outhost + ".local"
+                        res2 = os.system("sudo ping "+addrout + " -c 1 -w2 >/dev/null 2>&1")
+                        if res2 == 0:
+                                print outhost,"is alive"
+                                time.sleep(.5)
+                        else:
+                                print outhost,"is Dead"
+                                AlertMail(outhost)
+                                # mongodb update for failure
+				time.sleep(.5)
+
+
+
+def AlertMail(host):
+	import smtplib
+	sender = 'satheesh.selvanathan@tu-dortmund.de'
+	receivers = ['satheesh.selvanathan@web.de']	
+
+	message = """From: MINIS Alert Mail <alert@minis.mb.tu-dortmund.de>
+To: Administrator <admin@minis.mb.tu-dortmund.de>
+Subject : Module Failure Alert
+
+This is an automatic e-mail to Alert the failure of one or more MINIS modules. A module named """+ host +""" has failed. Check the MINIS webdesk for more details
+"""
+
+	smtpObj = smtplib.SMTP('unimail.tu-dortmund.de')
+	smtpObj.sendmail(sender, receivers, message)           
+	print "Successfully sent email"
+	
+
+
 
 Check()
