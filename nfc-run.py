7fc3a6b5edb1cd9fb95e3cfd198083f482efeb5f
satheesh90 <satheesh.retros@gmail.com>
2016-08-22 21:46:10 +0200

    update

--- nfc-run.py
+++ nfc-run.py
@@ -4,17 +4,13 @@ from minis.auto.NFCCheck import Status
 from minis.auto.NFC import Read
 
 import time
-
-Status()
+uid_list = ['None']
 
 while 1:
        	uid = Read()
-       	uid_list = [None]
-	
-	if uid in uid_list :
-		continue
+       
+       	if not uid in uid_list :
+       		uid_list.append(uid)
+       		print uid
 	else:	
-		print uid
-		uid_list.append(uid)
-		print "uid_list", uid_list
-		#break
+		continue
